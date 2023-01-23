c8825f50f7209ad1a56adf55da458f2d4f301c97
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-01-23 10:42:47 -0600

    Iteration 1

---
+++ js/keys.js
@@ -0,0 +1,3 @@
+const MAPBOX_API_KEY = "pk.eyJ1IjoibWFkbWFyY29zIiwiYSI6ImNsNWw0YWNobTBnY3AzanN4dzdpemhpeG0ifQ.OO7BegDPBkP3SV0CaLbnyQ";
+const OPENWEATHER_API_KEY = '59e1b99e68e6d9bdb350bf932c7ac068';
+const NWS_API_KEY = "User-Agent: (Codeup Weather Map project, joseph.mccomas430@gmail.com)"
