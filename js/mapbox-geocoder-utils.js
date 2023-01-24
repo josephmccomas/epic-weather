fdf9df0fa845ae44410eb510804e2928c6cf9ffd
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-01-24 11:28:40 -0600

    Iteration 2

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -1,30 +1,53 @@
 "use strict";
 
-//Skycons
+//wIcons
 
-var skycons = new Skycons({"color": "#FFFAFF"});
+const rain = import("img/rain.svg");
+const clearDay = import("img/clear-day.svg");
+const clearNight = import("img/clear-night.svg");
+const partlyCloudyDay = import("img/partly-cloudy-day.svg");
+const partlyCloudyNight = import("img/partly-cloudy-night.svg");
+const snow = import("img/snow.svg");
+const thunderstorm = import("img/thunderstorm.svg")
 
-skycons.add("animated-icon", Skycons.CLEAR_DAY);
+let wIcons = new wIcons({"color": "#FFFAFF"});
 
-skycons.play();
+wIcons.add("animated-icon", clearDay);
+
+wIcons.play();
+
+geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
+    // do something with results
+})
+function geocode(search, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
 
 //Some Global variables
 
-var longitude, latitude, timeHour, timeFull;
+let longitude, latitude, timeHour, timeFull;
 
 //Function to update weather information
 
 function updateWeather (json) {
 
-    longitude = json.coord.lon;
-    latitude = json.coord.lat;
+    longitude = json.coord.longitude;
+    latitude = json.coord.latitude;
 
     //AJAX request
 
-    $.getJSON('http://api.geonames.org/timezoneJSON?lat=' + latitude + '&lng=' + longitude + '&username=ayoisaiah', function(timezone) {
-        var rawTimeZone = JSON.stringify(timezone);
-        var parsedTimeZone = JSON.parse(rawTimeZone);
-        var dateTime = parsedTimeZone.time;
+    $.getJSON('https://api.weather.gov/openapi.json' + latitude + '&lng=' + longitude, function(timezone) {
+        let rawTimeZone = JSON.stringify(timezone);
+        let parsedTimeZone = JSON.parse(rawTimeZone);
+        let dateTime = parsedTimeZone.time;
         timeFull = dateTime.substr(11);
         $(".local-time").html(timeFull); //Update local time
         timeHour = dateTime.substr(-5, 2);
@@ -33,7 +56,7 @@ function updateWeather (json) {
     //Update Weather parameters and location
 
     $(".weather-condition").html(json.weather[0].description);
-    var temp = [(json.main.temp - 273.15).toFixed(0) + "°C", (1.8 * (json.main.temp - 273.15) + 32).toFixed(0) + "F"];
+    let temp = [(json.main.temp - 273.15).toFixed(0) + "°C", (1.8 * (json.main.temp - 273.15) + 32).toFixed(0) + "F"];
     $(".temp-celsius").html(temp[0]);
     $(".temp-fahrenheit").html(temp[1]);
     $(".temperature").click(function () {
@@ -44,42 +67,43 @@ function updateWeather (json) {
 
     //Update Weather animation based on the returned weather description
 
-    var weather = json.weather[0].description;
+
+    let weather = json.weather[0].description;
 
     if(weather.indexOf("rain") >= 0) {
-        skycons.set("animated-icon", Skycons.RAIN);
+        wIcons.set("animated-icon", rain);
     }
 
     else if (weather.indexOf("sunny") >= 0) {
-        skycons.set("animated-icon", Skycons.CLEAR_DAY);
+        wIcons.set("animated-icon", clearDay);
     }
 
     else if (weather.indexOf("clear") >= 0) {
         if (timeHour >= 7 && timeHour < 20) {
-            skycons.set("animated-icon", Skycons.CLEAR_DAY);
+            wIcons.set("animated-icon", clearDay);
         }
 
         else {
-            skycons.set("animated-icon", Skycons.CLEAR_NIGHT);
+            wIcons.set("animated-icon", clearNight);
         }
     }
 
     else if (weather.indexOf("cloud") >= 0) {
         if (timeHour >= 7 && timeHour < 20) {
-            skycons.set("animated-icon", Skycons.PARTLY_CLOUDY_DAY);
+            wIcons.set("animated-icon", partlyCloudyDay);
         }
 
         else {
-            skycons.set("animated-icon", Skycons.PARTLY_CLOUDY_NIGHT);
+            wIcons.set("animated-icon", partlyCloudyNight);
         }
     }
 
     else if (weather.indexOf("thunderstorm") >= 0) {
-        skycons.set("animated-icon", Skycons.SLEET);
+        wIcons.set("animated-icon", thunderstorm);
     }
 
     else if (weather.indexOf("snow") >= 0) {
-        skycons.set("animated-icon", Skycons.SNOW);
+        wIcons.set("animated-icon", snow);
     }
 }
 
@@ -90,7 +114,7 @@ if (navigator.geolocation) {
     //Return the user's longitude and latitude on page load using HTML5 geolocation API
 
     window.onload = function () {
-        var currentPosition;
+        let currentPosition;
         function getCurrentLocation (position) {
             currentPosition = position;
             latitude = currentPosition.coords.latitude;
@@ -98,9 +122,9 @@ if (navigator.geolocation) {
 
             //AJAX request
 
-            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=188b68e6b443a5380ce7ee0f0bb49cfc", function (data) {
-                var rawJson = JSON.stringify(data);
-                var json = JSON.parse(rawJson);
+            $.getJSON("https://api.weather.gov/openapi.json" + latitude + "&lon=" + longitude, function (data) {
+                let rawJson = JSON.stringify(data);
+                let json = JSON.parse(rawJson);
                 updateWeather(json); //Update Weather parameters
             });
         }
@@ -113,16 +137,16 @@ if (navigator.geolocation) {
 
     $("form").on("submit", function(event) {
         event.preventDefault();
-        var city = $(".find-forcast").val(); //Get value from form input
+        let city = $(".find-forecast").val(); //Get value from form input
         document.getElementById("my-form").reset();
 
         //AJAX Request
 
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=188b68e6b443a5380ce7ee0f0bb49cfc", function (data) {
-            var rawJson = JSON.stringify(data);
-            var json = JSON.parse(rawJson);
-            updateWeather(json); //Update Weather parameters
-        });
+        // $.getJSON("https://api.weather.gov/openapi.json" + city, function (data) {
+        //     let rawJson = JSON.stringify(data);
+        //     let json = JSON.parse(rawJson);
+        //     updateWeather(json); //Update Weather parameters
+        // });
     });
 }
 
@@ -143,22 +167,6 @@ else {
 //  *
 //  * EXAMPLE:
 //  *
-//  *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
-//  *      // do something with results
-//  *  })
-//  *
-//  */
-// function geocode(search, token) {
-//     let baseUrl = 'https://api.mapbox.com';
-//     let endPoint = '/geocoding/v5/mapbox.places/';
-//     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-//         .then(function(res) {
-//             return res.json();
-//             // to get all the data from the request, comment out the following three lines...
-//         }).then(function(data) {
-//             return data.features[0].center;
-//         });
-// }
 //
 //
 // /***
