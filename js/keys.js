c9302a747d0fcff472d001ecadd5191cf3b27368
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-01-25 14:00:01 -0600

    Iteration REBOOT v3

--- js/keys.js
+++ js/keys.js
@@ -1,4 +1,7 @@
 const MAPBOX_API_KEY = "pk.eyJ1IjoibWFkbWFyY29zIiwiYSI6ImNsNWw0YWNobTBnY3AzanN4dzdpemhpeG0ifQ.OO7BegDPBkP3SV0CaLbnyQ";
-const OPENWEATHER_API_KEY = '59e1b99e68e6d9bdb350bf932c7ac068';
+const OPEN_WEATHER_APPID = 'ac0db637bd01466b337b4cbd0d0707df';
+const OPENWEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + {location} + "&APPID=ac0db637bd01466b337b4cbd0d0707df"
 const NWS_API_KEY = "User-Agent: (Codeup Weather Map project, joseph.mccomas430@gmail.com)"
 const GOOGLE_MAPS_API_KEY = "4400dc9647mshcce19f8be2f36f1p14c09fjsn368b77085986"
+
+
